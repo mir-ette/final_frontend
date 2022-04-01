a7c7a663c4270b174888ecb1d09366e3a2884a37
mirette fayez khlil <rita.fayez21@gmail.com>
2022-04-01 15:31:24 +0200

    adding scrollup hndelin'cart empty

--- pharmatech.com/src/app/profile-admin/profile-admin.component.ts
+++ pharmatech.com/src/app/profile-admin/profile-admin.component.ts
@@ -3,85 +3,91 @@ import { DataService } from 'src/app/service/data.service';
 import { User } from 'src/app/user';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import jwt_decode from "jwt-decode";
+import { FormBuilder , FormGroup , Validators } from '@angular/forms';
 @Component({
   selector: 'app-profile-admin',
   templateUrl: './profile-admin.component.html',
   styleUrls: ['./profile-admin.component.css']
 })
+
 export class ProfileAdminComponent implements OnInit {
-  users: any;
+  data:any
   user = new User();
-  p: number = 1;
-  data: any;
-  role: any;
-  id:any;
-  constructor(private dataService: DataService,private route:ActivatedRoute,private router:Router) { }
+  form!:FormGroup;
+  submitted =false;
+  token :any = localStorage.getItem('token');
+  userDataFromToken : any
+  
 
-  ngOnInit(): void {
-    this.getUsersData()
+  constructor(private dataService: DataService,
+    private route:ActivatedRoute,
+    private router:Router,private formBuilder: FormBuilder,) { }
+
+  
+  createForm(){
+    this.form = this.formBuilder.group({
+    name:['' , [Validators.required , Validators.minLength(3)]],
+    email:['' , [Validators.required ,Validators.email]],
+    password:['' , [Validators.required ,Validators.minLength(6)]],
+    city:['' , [Validators.required , Validators.minLength(3)]],
+    street:['' , [Validators.required , Validators.minLength(3)]],
+    });
   }
- getUsersData() {
+  ngOnInit(): void {
+ 
+    console.log(this.route.snapshot.params['id']);
+    this.getUserrData()
+    this.createForm();
+    if(this.token){
+      this.userDataFromToken = jwt_decode(this.token);
+    }
+  }
 
+  getUserrData(){
+    this.dataService.getUserById(this.userDataFromToken.user_id).subscribe(res => {
+      console.log(res);
+      this.data = res
+      this.user.name = this.data.name
+      this.user.email = this.data.email;
+      this.user.password = this.data.password;
+      this.user.city = this.data.city;
+      this.user.street = this.data.street;
+    console.log(res);
     
-      this.dataService.getUsersData().subscribe(res => {
-        this.users = res;
-      });
-      console.log('yes');
+  });
+
+  }
 
  
-   }
-
-  // getUsersData() {
-
-  //   if (this.data['role'] == "admin") {
-  //     this.dataService.getUsersData().subscribe(res => {
-  //       this.users = res;
-  //     });
-  //     console.log('yes');
-
-  //   }
-  //   else {
-  //     console.log('no');
-
-
-  //   }
-  // }
-  updateUserData(){
-    this.dataService.updateUserData(this.id,this.user).subscribe(res=>{
-       this.data=res;
-       this.user=this.data
-    })
-    //this.router.navigate(['/users'])
+  updateUserrData(){
+    this.submitted = true;
+    if(this.form.invalid){
+      return;
+    }
+    else{
+      this.dataService.updateUserData(this.userDataFromToken.user_id , this.user).subscribe(res => {
+      });
+    }
   }
 
-  getAdminData(){
-    this.dataService.getUserById(this.id).subscribe(res=>{
-  //     // console.log(res); 
-     this.data=res;
-      this.user=this.data;
-      
-    }) 
-  
-  
-   }
-
-
-
-
-
-
-
-
-
-
-
-  
-  deleteUserData(id: any) {
-    this.dataService.deleteUserData(id).subscribe(res => {
-      this.getUsersData();
-    })
-
+  get f(){
+    return this.form.controls;
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
